1f86bc51cf4da2e98b298e1ad72c4025fd148cfd
Ada <Ada@PROBOOK>
2025-09-06 17:32:38 +0700

    Add project files.

---
+++ Academy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//StackOverflow форум
+
+namespace Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human= new Human("Schwartzenegger", "Arnold", 78);
+            Console.WriteLine(human);
+
+            Student student = new Student("Colombo", "Franco", 91, "Heavy Metal", "LA", 98, 100);
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ Academy/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+    internal class Student: Human
+    {
+        public string Speciality { get; set; }
+        public string Group { get; set; }
+        public double Rating { get; set; }
+        public double Attendance { get; set; }
+
+        // Constructors
+        public Student(string lastName, string firstName, int age,
+            string speciality, string qroup, double rating, double attendance):base(lastName, firstName, age)
+        {
+            Speciality = speciality;
+            Group = qroup;
+            Rating = rating;
+            Attendance = attendance;
+            Console.WriteLine($"SConstructor:\t {GetHashCode().ToString("X")}");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" {Speciality} {Group} {Rating} {Attendance}";
+        }
+    }
+}
+
